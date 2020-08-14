31d045c0a1e6c83196cde47b321e38a6b24f132b
marcosvanille <marcosvanillle@gmail.com>
2020-08-13 21:41:10 -0300

    [UPDATE]

---
+++ troca-de-oleo.js
@@ -0,0 +1,13 @@
+// var bb = document.getElementById('inputMarca')
+// var cc = document.getElementById('inputCarro')
+//
+// bb.onclick = function (){
+// console.log(cc);
+// }
+// $(function(){
+//     $("#B").html("jquery aaa")
+// });
+
+$( "#inputMarca #B" ).click(function() {
+    console.log('sdsd')
+});
